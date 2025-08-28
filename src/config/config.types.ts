346707f9c664571146f18f511ea3773c827713c3
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-28 16:13:59 +0800

    database configuration with typeorm and env variables

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -7,4 +7,9 @@ export interface ConfigType {
 
 export const appConfigSchema = Joi.object({
   APP_MESSAGE_PREFIX: Joi.string().default('Hello'),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5434),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
 });
