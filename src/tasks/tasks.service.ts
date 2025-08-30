8d753903c2d8193aacaeb4536bd455dc6889209f
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-30 20:36:03 +0800

    ensuring unique labels with a helper method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,9 +30,9 @@ export class TasksService {
   }
 
   public async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    // await this.tasksRepository.create({
-    //   ...createTaskDto,
-    // });
+    if (createTaskDto.labels) {
+      createTaskDto.labels = this.getUniqueLabels(createTaskDto.labels);
+    }
     return await this.tasksRepository.save(createTaskDto);
   }
 
@@ -45,6 +45,10 @@ export class TasksService {
       !this.isValidStatusTransition(task.status, updateTaskDto.status)
     ) {
       throw new WrongTaskStatusException();
+    }
+
+    if (updateTaskDto.labels) {
+      updateTaskDto.labels = this.getUniqueLabels(updateTaskDto.labels);
     }
 
     Object.assign(task, updateTaskDto);
@@ -62,6 +66,10 @@ export class TasksService {
     return await this.tasksRepository.save(task);
   }
 
+  public async deleteTask(task: Task): Promise<void> {
+    await this.tasksRepository.delete(task);
+  }
+
   private isValidStatusTransition(
     currentStatus: TaskStatus,
     newStatus: TaskStatus,
@@ -75,7 +83,10 @@ export class TasksService {
     return statusOrder.indexOf(currentStatus) <= statusOrder.indexOf(newStatus);
   }
 
-  public async deleteTask(task: Task): Promise<void> {
-    await this.tasksRepository.delete(task);
+  private getUniqueLabels(
+    labelDtos: CreateTaskLabelDto[],
+  ): CreateTaskLabelDto[] {
+    const uniqueNames = [...new Set(labelDtos.map((label) => label.name))];
+    return uniqueNames.map((name) => ({ name }));
   }
 }
