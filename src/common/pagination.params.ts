5259d3e72523451e1fbe455a718b657243711ab7
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-01 08:13:19 +0800

    implementing pagination parameters and response types

---
+++ src/common/pagination.params.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+export class PaginationParams {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Max(10000)
+  limit: number = 10;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  offset: number = 0;
+}
