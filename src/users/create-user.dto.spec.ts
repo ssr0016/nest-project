5705a6695ce8ce0b532663b36a0666cd0b1bbf41
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-01 20:32:41 +0800

    adding detailed unit test cases

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -2,21 +2,36 @@ import { validate } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 describe('CreateUserDto', () => {
+  let dto = new CreateUserDto();
+
+  beforeEach(() => {
+    dto = new CreateUserDto();
+    dto.email = 'test@test.com';
+    dto.name = 'test';
+    dto.password = '123456';
+  });
+
   it('2+2=4', () => {
     expect(2).toBe(2); //Assertion
   });
 
   it('should validate complete valid data', async () => {
-    // 3xA
     // Arrange
-    const dto = new CreateUserDto();
-    dto.email = 'test@test.com';
-    dto.name = 'test';
-    dto.password = '123456';
-
     // Act
     const errors = await validate(dto);
     // Assert
     expect(errors.length).toBe(0);
+  });
+
+  it('should fail on invalid email', async () => {
+    // Arrance
+    dto.email = 'test';
+    // Act
+    const errors = await validate(dto);
+    // Assert
+    expect(errors.length).toBeGreaterThan(0);
+    // console.log(errors);
+    expect(errors[0].property).toBe('email');
+    expect(errors[0].constraints).toHaveProperty('isEmail');
   });
 });
