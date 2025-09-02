898e2092d875ce037f44ae90b9b39f731df39c53
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-02 13:32:23 +0800

    implementing secure login authentication

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,14 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../create-user.dto';
 import { User } from '../user.entity';
+import { PasswordService } from '../password/password.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly passwordService: PasswordService,
   ) {}
 
   public async register(createUserDto: CreateUserDto): Promise<User> {
@@ -27,6 +33,22 @@ export class AuthService {
     // 3) Return the token
 
     return user;
+  }
+
+  public async login(email: string, password: string): Promise<string> {
+    const user = await this.userService.findOneByEmail(email);
+
+    // 1) There is no such user
+    // 2) Password is invalid
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!(await this.passwordService.verify(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.generateToken(user);
   }
 
   private generateToken(user: User): string {
