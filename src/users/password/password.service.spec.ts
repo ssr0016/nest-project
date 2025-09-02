82d2e0400fd60824601e7e7de77f29765b0d7be0
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-02 11:14:13 +0800

    testing password service - mock & spy

--- src/users/password/password.service.spec.ts
+++ src/users/password/password.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordService } from './password.service';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(),
+  compare: jest.fn(),
+}));
 
 describe('PasswordService', () => {
   let service: PasswordService;
@@ -14,5 +20,22 @@ describe('PasswordService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // hash()
+  // plain text -> hashed text
+  // for the same input -> the same output
+  // 1234567 -> sadfasf123123sdf
+  // ---------
+  // bcrypt.has -> was called
+  // -> password was passed to it & salt rounds
+  // mocks & spy
+  it('should hash password', async () => {
+    const mockHash = 'hashed_password';
+    (bcrypt.hash as jest.Mock).mockResolvedValue(mockHash);
+    const password = 'password123';
+    const result = await service.hash(password);
+    expect(bcrypt.hash).toHaveBeenCalledWith(password, 10);
+    expect(result).toBe(mockHash);
   });
 });
